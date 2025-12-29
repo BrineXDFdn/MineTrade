12fab3a474a36994bfe462e0ec87e1e088aad716
MineTrade <ftbyt0091@gmail.com>
2025-12-29 22:24:28 +0600

    Update ImageScript.js

--- Games/ImageScript.js
+++ Games/ImageScript.js
@@ -1,1 +1,31 @@
+const popup = document.getElementById('popup');
+const popupImg = document.getElementById('popup-img');
+const downloadLink = document.getElementById('download-link');
+
+function openPopup(src) {
+  popup.style.display = 'flex';
+  popupImg.src = src;
+  downloadLink.onclick = () => forceDownload(src);
+}
+
+function closePopup() {
+  popup.style.display = 'none';
+  popupImg.src = '';
+}
+
+function forceDownload(src) {
+  const link = document.createElement('a');
+  link.href = src;
+  link.download = src.split('/').pop();
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+popup.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    closePopup();
+  }
+});
+
 
